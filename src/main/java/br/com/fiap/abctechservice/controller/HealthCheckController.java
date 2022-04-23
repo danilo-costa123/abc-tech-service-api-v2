c163fc3a58cf1a4c5731bacc04704bf0e2a25fea
danilo-costa123 <danilo.sa01@gmail.com>
2022-04-23 00:07:59 -0300

    ajuste health check

--- src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -1,5 +1,12 @@
 package br.com.fiap.abctechservice.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +19,13 @@ public class HealthCheckController {
 	@GetMapping()
 	public ResponseEntity<String> status() {
 		return ResponseEntity.ok("Sucesso!");
+	}
+
+	@GetMapping("version")
+	public ResponseEntity<String> version() throws FileNotFoundException, IOException, XmlPullParserException {
+		MavenXpp3Reader reader = new MavenXpp3Reader();
+		Model model = reader.read(new FileReader("pom.xml"));
+		return ResponseEntity.ok(model.getName() + " - " + model.getVersion());
 	}
 
 }
