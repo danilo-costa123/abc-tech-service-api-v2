b27c6b6500657f4377508c32f57e4fb00ce3df00
danilo-costa123 <danilo.sa01@gmail.com>
2022-04-23 20:20:00 -0300

    ajuste health check

--- src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -7,14 +7,22 @@ import java.io.IOException;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.abctechservice.aplication.PropertiesComponent;
 
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
+
+	private PropertiesComponent propertiesComponent;
+
+	public HealthCheckController(@Autowired PropertiesComponent propertiesComponent) {
+		this.propertiesComponent = propertiesComponent;
+	}
 
 	@GetMapping()
 	public ResponseEntity<String> status() {
@@ -22,19 +30,8 @@ public class HealthCheckController {
 	}
 
 	@GetMapping("version")
-	public ResponseEntity<String> version() {
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = new Model();
-		try {
-			model = reader.read(new FileReader("pom.xml"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
-		}
-		return ResponseEntity.ok(model.getName() + " - " + model.getVersion());
-	}
+    public ResponseEntity<String> version() {
+        return ResponseEntity.ok(this.propertiesComponent.getName() + " - " + this.propertiesComponent.getVersion());
+    }
 
 }
